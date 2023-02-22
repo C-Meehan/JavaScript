85122964b82857f45f948b61529289fced5f3480
Christopher Meehan <chrismeehan91@gmail.com>
2023-02-22 15:56:08 -0600

    first commit

---
+++ React/axios-pokemon/src/components/Pokemon.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const Pokemon = () => {
+
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        .then(response => setPokemon(response.data.results))
+    }, []);
+    console.log(pokemon);
+
+    return (
+        <div>
+            <div>Pokemon List</div>
+            <ol>
+            {
+                pokemon.map((pokemon, index) => {
+                    return (
+                            <li key={index}>{pokemon.name}</li>
+                            )
+                        })
+            }
+            </ol>
+        </div>
+    )
+}
+
+export default Pokemon
